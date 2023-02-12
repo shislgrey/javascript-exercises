117b5a42fae5bdc4a2dfe9120e014d511a8ecb9b
Grace <shisler.grace@gmail.com>
2023-02-11 20:36:56 -0800

    completed solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (str, reps) {
+    let newstr = '';
+    if (reps < 0) {
+        newstr = 'ERROR';
+    } else {
+        for (let counter = 0; counter < reps; counter++) {
+            newstr = newstr.concat('', str);
+        }
+    }
+    return newstr;
 };
 
 // Do not edit below this line
