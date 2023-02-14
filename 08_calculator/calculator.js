37f8ed653b7112ac6dae76c92db3fdec97dbe13e
Grace <shisler.grace@gmail.com>
2023-02-13 23:24:35 -0800

    solutions to add, subtract, sum, multiply, power
    Co-authored-by: Pavel Kirkovsky <pavel@kirkovsky.com>

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,27 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  // return array.reduce((a, b) => a + b, 0);
+  return array.reduce((a, b) => add(a, b), 0);
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  // return array.reduce((a, b) => a * b, 1);
+  return array.reduce((a, b) => a * b);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function () {
+
 };
 
 // Do not edit below this line
