aead565725a59afe0e2ab8bdff66b0b66f668e7c
Grace <shisler.grace@gmail.com>
2023-02-13 23:58:06 -0800

    pseudocode for sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+    // expect(sumAll(1, 4)).toEqual(10);
+    // 1 + 2 + 3 + 4 = 10
+    // a           b
 
+    // for i = a, i <= b, i++
+    // return i.reduce((i, i+1) => (i + (i+1), 0))
 };
 
 // Do not edit below this line
