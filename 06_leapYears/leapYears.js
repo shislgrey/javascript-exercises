7759ee785792f1ae612f8403f63d326bf6b97e51
Pavel <pavel@kirkovsky.com>
2023-02-16 23:53:35 -0800

    Alternative solutions for leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,8 +1,20 @@
 const leapYears = function (year) {
-    if (year % 4 == 0) {
-        if (year % 100 == 0 && year % 400 != 0) { return false; }
-        return true;
-    } else { return false }
+    // if (year % 4 == 0) {
+    //     if (year % 100 == 0 && year % 400 != 0) { return false; }
+    //     return true;
+    // } else { return false }
+
+    // Slightly cleaner, using a guard clause style
+    // https://blog.webdevsimplified.com/2020-01/guard-clauses/
+    // if (year % 100 == 0 && year % 400 != 0) { return false; }
+    // if (year % 4 == 0) { return true; }
+    // return false;
+
+    // Most compact - don't be shy about "cheating" with builtins
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
+    if (new Date(year, 1, 29).getDate() === 29) { return true; }
+    return false;
+
 };
 
 // Do not edit below this line
