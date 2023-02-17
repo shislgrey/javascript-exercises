5246442e0156819d850b515f62c8b5bda0fca9c2
Grace <shisler.grace@gmail.com>
2023-02-16 23:25:44 -0800

    minor refactoring for solution to leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,10 +1,6 @@
 const leapYears = function (year) {
     if (year % 4 == 0) {
-        if (year % 100 == 0 && year % 400 == 0) {
-            return true;
-        } else if (year % 100 == 0 && year % 400 != 0) {
-            return false;
-        }
+        if (year % 100 == 0 && year % 400 != 0) { return false; }
         return true;
     } else { return false }
 };
