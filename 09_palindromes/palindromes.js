1e04534d773992da31f78c6cbf303c7a042817f3
Grace <shisler.grace@gmail.com>
2023-02-26 23:39:16 -0800

    sketching out function palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let pal = string.toLowerCase().replace(/[^A-Za-z0-9]/g, '');
+    let ind;
+    let rome;
+    if (pal.length % 2 != 1) {
+        ind = pal.slice(pal.length / 2);
+        rome = pal.slice(0, pal.length / 2).split('').reverse().join('');
+    } else {
+        ind = pal.slice(Math.round(pal.length / 2));
+        rome = pal.slice(0, Math.round(pal.length / 2)).split('').reverse().join('');
+    }
 };
 
 // Do not edit below this line
