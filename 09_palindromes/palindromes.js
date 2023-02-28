b5afcb494269f02cdf1aeb5ad922a6c2b63d65b4
Grace <shisler.grace@gmail.com>
2023-02-27 23:02:16 -0800

    completed palindromes function, all tests passing

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,14 +1,15 @@
 const palindromes = function (string) {
-    let pal = string.toLowerCase().replace(/[^A-Za-z0-9]/g, '');
-    let ind;
-    let rome;
-    if (pal.length % 2 != 1) {
-        ind = pal.slice(pal.length / 2);
-        rome = pal.slice(0, pal.length / 2).split('').reverse().join('');
-    } else {
-        ind = pal.slice(Math.round(pal.length / 2));
-        rome = pal.slice(0, Math.round(pal.length / 2)).split('').reverse().join('');
+    let cleanString = string.toLowerCase().replace(/[^A-Za-z0-9]/g, '');
+    let firstHalf;
+    let secondHalf;
+    if (cleanString.length % 2 == 0) {
+        firstHalf = cleanString.slice(cleanString.length / 2);
+        secondHalf = cleanString.slice(0, cleanString.length / 2).split('').reverse().join('');
+    } else if (cleanString.length % 2 == 1) {
+        firstHalf = cleanString.slice(Math.round(cleanString.length / 2));
+        secondHalf = cleanString.slice(0, Math.round(cleanString.length / 2) - 1).split('').reverse().join('');
     }
+    if (firstHalf == secondHalf) { return true } else { return false }
 };
 
 // Do not edit below this line
