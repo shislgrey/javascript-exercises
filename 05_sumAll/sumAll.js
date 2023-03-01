67596d46f56d8c763062a338fff9484228843a5f
Pavel Kirkovsky <pavel@kirkovsky.com>
2023-02-28 20:35:59 -0800

    concise but incomplete solution for sumAll (FIXME)

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,16 +1,19 @@
 const sumAll = function (a, b) {
     if ((a >= 0) && (b >= 0) && (typeof a === "number") && typeof b === "number") {
-        let sum = 0;
-        if (a < b) {
-            for (i = a; i <= b; i++) {
-                sum += i;
-            }
-        } else {
-            for (i = b; i <= a; i++) {
-                sum += i;
-            }
-        }
-        return sum;
+        // let sum = 0;
+        // if (a < b) {
+        //     for (i = a; i <= b; i++) {
+        //         sum += i;
+        //     }
+        // } else {
+        //     for (i = b; i <= a; i++) {
+        //         sum += i;
+        //     }
+        // }
+        // return sum;
+
+        // FIXME: make this work with larger number first
+        return (b / 2) * (a + b) 
     }
     return "ERROR";
     // for i = a, i <= b, i++
