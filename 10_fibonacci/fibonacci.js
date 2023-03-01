1f8810adf5c0fc3e672b01ea7ffb5fc9141753c0
Grace <shisler.grace@gmail.com>
2023-02-28 20:55:35 -0800

    completed solution for fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+    num = parseInt(num);
+    let fib = [0, 1];
+    if (num <= 0) {
+        return "OOPS";
+    }
+    else if (num >= fib.length) {
+        for (let i = fib.length; i <= num; i++)
+            fib.push((fib.at(-1)) + (fib.at(-2)));
+    }
+    return fib[num];
 };
-
+fibonacci("2")
 // Do not edit below this line
 module.exports = fibonacci;
