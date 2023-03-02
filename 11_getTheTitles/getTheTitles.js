edf8d8f342fa9a4cda2674392f8f80f8c9392a18
Pavel Kirkovsky <pavel@kirkovsky.com>
2023-03-01 18:02:46 -0800

    Extra credit for getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,4 +1,5 @@
 const getTheTitles = function (books) {
+    //TODO: use Array's .map method, and do not explicitly create an intermediate array. Return the result directly
     let bookNames = [];
     books.forEach(book => bookNames.push(book.title));
     return bookNames;
