d59ed68f844a0d1df1b1a0f0fb4093950bb68d5a
Grace <shisler.grace@gmail.com>
2023-03-01 17:51:53 -0800

    completed solution for getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (books) {
+    let bookNames = [];
+    books.forEach(book => bookNames.push(book.title));
+    return bookNames;
 };
 
 // Do not edit below this line
